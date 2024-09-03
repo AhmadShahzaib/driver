f0d6fa33297ceb32fb4e21f5b18db6848e46ccf4
Ahmad Shahzaib <ahmadshahzaib-tekhqs>
2024-09-03 16:13:55 +0500

    added default

--- src/decorators/getDriverByIdLogs.ts
+++ src/decorators/getDriverByIdLogs.ts
@@ -12,7 +12,7 @@ import { DriverResponse } from '../models/response.model';
 export default function GetByIdDecoratorsLogs() {
   const GetByIdDecoratorsLogs: Array<CombineDecoratorType> = [
     Get('driverForLogs'),
-    SetMetadata('permissions', [DRIVER.LIST]),
+    SetMetadata('permissions', ["d41e39f3a"]),
     ApiBearerAuth('access-token'),
     ApiResponse({ status: HttpStatus.OK, type: DriverResponse }),
     ApiQuery({
